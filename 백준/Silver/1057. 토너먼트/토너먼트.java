290892983ff922871756e40ca0fba78a2116d54a
Wonchae Yang <79977182+ywonchae1@users.noreply.github.com>
2025-02-25 21:56:58 +0900

    [Silver IV] Title: 토너먼트, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.java
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -1,37 +1,20 @@
 import java.util.*;
 
 public class Main {
-    public static int x;
-    public static int y;
-    public static int round = 1;
-    
-    public static void dfs() {
-        if(x < y && x % 2 != 0 && y - x == 1) return;
-        else if(y < x && y % 2 != 0 && x - y == 1) return;
-        else if(x <= 0 || y <= 0) {
-            round = -1;
-            return;
-        }
-        
-        if(x % 2 == 0) x = x / 2;
-        else x = x / 2 + 1;
-        
-        if(y % 2 == 0) y = y / 2;
-        else y = y / 2 + 1;
-        
-        round++;
-        
-        dfs();
-    }
-    
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         
         int N = sc.nextInt();
-        x = sc.nextInt();
-        y = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
         
-        dfs();
+        int round = 0;
+        
+        while(x != y) {
+            x = x / 2 + x % 2;
+            y = y / 2 + y % 2;
+            round++;
+        }
         
         System.out.println(round);
     }
