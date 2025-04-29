c6cb58d28d85488d402df16991caaacc8601af27
Wonchae Yang <ywonchae62@gmail.com>
2025-04-29 14:59:15 +0900

    [Silver I] Title: IOIOI, Time: 504 ms, Memory: 76464 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -5,21 +5,27 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        int n = 3 + 2 * (N - 1);
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < n; i++) {
-            if(i % 2 == 0) sb.append('I');
-            else sb.append('O');
-        }
         int M = sc.nextInt();
 
         String S = sc.next();
         int answer = 0;
-
-        for(int i = 0; i < M - n + 1; i++) {
-            if(S.charAt(i) == 'O') continue;
-            String str = S.substring(i, i + n);
-            if(str.contentEquals(sb)) answer++;
+        int cnt = 0;
+        
+        int i = 0;
+        while(i < M - 2) {
+            String str = S.substring(i, i + 3);
+            if(str.equals("IOI")) {
+                cnt++;
+                i += 2;
+                
+                if(cnt == N) {
+                    answer++;
+                    cnt--;
+                }
+            } else {
+                cnt = 0;
+                i++;
+            }
         }
 
         System.out.println(answer);
